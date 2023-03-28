95b55de924cfc808458fe223ebda1a37531e2e30
seo-jihyo <ohyiif@gmail.com>
2023-03-29 08:57:54 +0900

    Bj0329

--- ex01/src/BJ1330.java
+++ ex01/src/BJ1330.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 
 public class BJ1330 {
 
-	
+		public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+			
+			int A = in.nextInt();
+			int B = in.nextInt();
+			
+			in.close();
+	        
+			if(A>B) System.out.println(">");
+			else if(A<B) System.out.println("<");
+			else System.out.println("=="); 
+		
+	}
 }
