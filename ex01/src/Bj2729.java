e3c65781b43e3f2c2eaf661677f221251d3c8ad6
seo-jihyo <ohyiif@gmail.com>
2023-03-29 17:13:17 +0900

    0329 BJ

---
+++ ex01/src/Bj2729.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bj2729 {
+	
+		public static void main(String[] args) {
+	 
+			Scanner in = new Scanner(System.in);	
+			int a = in.nextInt();
+			
+			in.close();		
+			
+			for(int i = 1; i<10;i++) {
+				System.out.println(a+" * "+i+" = "+(a*i));
+				
+		}
+	}
+}
