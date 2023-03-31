41906941f5384ece1d554d6ca5083da9f203f37a
seo-jihyo <ohyiif@gmail.com>
2023-03-31 17:39:13 +0900

    0331-Bj

--- ex01/src/Bj2441.java
+++ ex01/src/Bj2441.java
@@ -2,33 +2,20 @@ import java.util.Scanner;
 
 public class Bj2441 {
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		int num = sc.nextInt();
-
-//		for (int i = 1; i <= num; i++) {
-//
-//			for (int j = 1; j < num-1; j++) {
-//				System.out.print(" ");
-//				
-//			}
-//			for (int k = num; k >= 1; k--) {
-//				System.out.println("*");
-//				
-//			}
-
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 
 		for (int i = 1; i <= num; i++) {
 
-			for (int j = 1; j <= i; j++) {
+			for (int j =1; j < i; j++) {
 				System.out.print(" ");
 
 			}
-			for (int k = num; k >= 1; k--) {
-				System.out.println("*");
-			}
+			for (int j = num; j>= i;j--) {
+				System.out.print("*");
 
+			}
+			System.out.println();
 		}
 	}
 }
