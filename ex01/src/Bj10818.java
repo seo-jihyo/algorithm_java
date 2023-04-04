654eb82c3d591563b85f26b4fb69134d8972ef4d
seo-jihyo <ohyiif@gmail.com>
2023-04-05 08:54:03 +0900

    0404-BJ

--- ex01/src/Bj10818.java
+++ ex01/src/Bj10818.java
@@ -3,20 +3,7 @@ import java.util.Scanner;
 
 public class Bj10818 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-	
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int[] num = new int[N];
-		 
-		for (int i = 0; i < num.length; i++){
-			num[i] = sc.nextInt();
-			
-		}
-		Arrays.sort(num);
-		System.out.println(num[0]+" " + num[N-1]);
-=======
+
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -28,8 +15,7 @@ public class Bj10818 {
 		}
 		Arrays.sort(num);
 		System.out.println(num[0]+" "+num[N-1]);
-			
->>>>>>> 989176502310805bc4d174479d3efdf2bb3545cd
+
 		
 	}
 }
