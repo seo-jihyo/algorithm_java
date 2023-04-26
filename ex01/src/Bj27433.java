3eca31a6125f9821eb1139e49f473c5b383a0f11
seo-jihyo <ohyiif@gmail.com>
2023-04-26 17:03:34 +0900

    0426_BJ

---
+++ ex01/src/Bj27433.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj27433 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int fac = 1;
+		for (int i = n; i > 0; i--) {
+			fac *= i;
+		}
+		System.out.println(fac);
+
+	}
+}
