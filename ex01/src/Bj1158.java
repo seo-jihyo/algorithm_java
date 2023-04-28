4235dacdd8d98b1c02bb328344ae53504a899f39
seo-jihyo <ohyiif@gmail.com>
2023-04-28 18:01:08 +0900

    0428-BJ

---
+++ ex01/src/Bj1158.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Bj1158 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); //사람 수
+		int K = Integer.parseInt(st.nextToken()); //제거될 순서
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		for (int i = 1; i <= N; i++) {
+			q.offer(i);
+		}
+
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		
+		while (q.size() != 1) {
+			for (int i = 0; i < K-1; i++) {
+				q.offer(q.peek());
+			}
+			sb.append(q.poll()+",");
+		}
+		
+		sb.append(q.poll() + ">");
+		
+		bw.write(sb.toString() + "\n" );
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
--- ex01/src/Bj2748.java
+++
@@ -1,4 +0,0 @@
-
-public class Bj2748 {
-
-}
