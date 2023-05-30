6b3177616ab7714b82ab2338d1cb6cadd9d03819
seo-jihyo <ohyiif@gmail.com>
2023-05-30 14:54:19 +0900

    boj-0530

--- ex01/src/Bj10773.java
+++ ex01/src/Bj10773.java
@@ -4,25 +4,27 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Bj10773 {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int sum = 0;
-		Stack<Integer> stack = new Stack<>();
-		
-		int K = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < K; i++) {
-			  int number = Integer.parseInt(br.readLine());
-		        if (number==0){
-		            stack.pop();
-		        }else {
-		            stack.push(number);
-		        }
-		    }
-		    for (int i : stack){
-		        sum += i;
-		    }
-		    System.out.println(sum);
-		    }
+		public static void main(String[] args) throws NumberFormatException, IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			Stack<Integer> stack = new Stack<>();
+			
+			int K = Integer.parseInt(br.readLine());
+			
+			for (int i = 0; i < K; i++) {
+				int num = Integer.parseInt(br.readLine());
+				
+				if (num == 0) {
+					stack.pop();
+				} else {
+					stack.push(num);
+				}
+			}// end for
+			
+			int sum = 0;
+			for (Integer i : stack) {
+				sum +=i;
+			}// end for
+			System.out.println(sum);
+			}
 }
--- ex01/src/Boj.java
+++ ex01/src/Boj.java
@@ -1,71 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Boj{
-
-	public static Stack<Integer> stack = new Stack<>();
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuffer sb = new StringBuffer();
-		int N = sc.nextInt();
-
-		for (int i = 0; i < N; i++) {
-			String s = sc.next();
-
-			switch (s) {
-			case "push":
-				int x = sc.nextInt();
-				stack.push(x);
-				break;
-
-			case "pop":
-				sb.append(pop()).append('\n');
-				break;
-
-			case "size":
-				sb.append(size()).append('\n');
-				break;
-
-			case "empty":
-				sb.append(empty()).append('\n');
-				break;
-
-			case "top":
-				sb.append(top()).append('\n');
-				break;
-			}
-		}
-		System.out.println(sb);
-	}
-
-	public static int pop() {
-		if (stack.isEmpty()) {
-			return -1;
-		} else {
-			int tmp = stack.peek();
-			stack.pop();
-			return tmp;
-		}
-	}
-
-	public static int size() {
-		return stack.size();
-	}
-
-	public static int empty() {
-		if (stack.isEmpty()) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-
-	public static int top() {
-		if (stack.isEmpty()) {
-			return -1;
-		} else {
-			return stack.peek();
-		}
-	}
+public class Boj {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int sum = 0;
+		Stack<Integer> stack = new Stack<>();
+		
+		int K = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < K; i++) {
+			  int number = Integer.parseInt(br.readLine());
+		        if (number==0){
+		            stack.pop();
+		        }else {
+		            stack.push(number);
+		        }
+		    }
+		    for (int i : stack){
+		        sum += i;
+		    }
+		    System.out.println(sum);
+		    }
 }
